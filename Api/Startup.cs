fa68bea304c0d4e34947a9949e2864ddc4b68513
Jack <jack.parra253@gmail.com>
2020-06-17 11:42:53 -0500

    Se agrega la configuración de servicio EF con sql lite

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Datos;
+
 
 namespace Api
 {
@@ -26,6 +29,7 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<IncapacidadesContext>(options => options.UseSqlite(Configuration.GetConnectionString("IncapacidadesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Datos/IncapacidadesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Datos
+{
+    public class IncapacidadesContext : DbContext
+    {
+        public IncapacidadesContext(DbContextOptions<IncapacidadesContext> options): base (options)
+        {
+            
+        }
+
+        
+    }
+}
