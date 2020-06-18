172167db1d7ed350e4e8df2cc9670cbd9e1d8a8f
Jack <jack.parra253@gmail.com>
2020-06-18 16:09:09 -0500

    front end Empleado: se agrega select con los empleados y tajeta detalle empleado

--- Api/wwwroot/js/site.js
+++ Api/wwwroot/js/site.js
@@ -1,8 +1,61 @@
-const uri = 'WeatherForecast';
+const uriEmpleado = 'Empleado';
+const empleados = [];
 
-function get() {
-    fetch(uri)
-    .then(response => response.json())
-    .then(data => _displayItems(data))
-    .catch(error => console.error('Unable to get WeatherForecast.', error));
+function llenarSelectEmpleados() {
+    obtenerEmpleados();
+}
+
+function obtenerEmpleados() {
+    fetch(uriEmpleado)
+        .then(response => response.json())
+        .then(data => agregarEmpleados(data))
+        .catch(err => console.log("Error" + err));
+}
+
+function agregarEmpleados(data) {
+    data.forEach(element => empleados.push(element));
+    let select = document.getElementById('empleados');
+
+    for (var i = 0; i < empleados.length; i++) {
+        var option = document.createElement("option");
+        option.value = empleados[i].id;
+        option.text = `${empleados[i].nombres} ${empleados[i].apellidos}`;
+        select.appendChild(option);
+    }
+
+    mostrarDetalleEmpleado();
+}
+
+function mostrarDetalleEmpleado() {
+    const idEmpleado = obtenerValorEmpleadoSeleccionado();
+    const empleado = obtenerEmpleado(idEmpleado);
+
+    let empleadoNombres = document.getElementById('empleadoNombres');
+    empleadoNombres.innerText = `${empleado.nombres} ${empleado.apellidos}`;
+
+    let empleadoSalario = document.getElementById('empleadoSalario');
+    empleadoSalario.innerText = `Tipo salario: ${transformarTipoSalario(empleado.tipoSalario)} - Salario: ${empleado.salario.cantidad} ${empleado.salario.moneda} - Salario diario: ${empleado.salarioDiario.cantidad} ${empleado.salarioDiario.moneda}  `;
+
+    empleadoTipoSalario = document.getElementById('empleadoTipoSalario');
+    empleadoTipoSalario.innerText = ``;
+
+}
+
+function transformarTipoSalario(tipoSalario){
+    if(tipoSalario == 1)
+        return 'Ley 50';
+    else if(tipoSalario == 2)
+        return 'Integral';
+}
+
+function obtenerValorEmpleadoSeleccionado(){
+    let select = document.getElementById("empleados");
+
+    return select.options[select.selectedIndex].value;
+}
+
+function obtenerEmpleado(id){
+    let empleado = empleados.filter(empleado => empleado.id == id);
+
+    return empleado[0];
 }
